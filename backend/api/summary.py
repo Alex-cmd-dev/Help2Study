77886fdf97c3ba60704132c7609da3173a49a2a3
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-10 16:18:10 -0600

    file processing added

--- backend/api/summary.py
+++ backend/api/summary.py
@@ -1,4 +1,14 @@
 def processfile(request):
     if request.method == 'POST':
-            uploaded_file = request.FILES['file']
-            file_content = uploaded_file.read()
+        uploaded_file = request.FILES['file']
+        # Get the Content-Type header from the request
+        content_type = request.headers.get('Content-Type', '')
+        # Check if the file is a text file
+        if not content_type.startswith('text/'):
+            return "Only text files are allowed"
+            
+        # Proceed with processing...
+        file_content = uploaded_file.read()
+
+        
+        
