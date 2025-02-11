9bd4fe8dcec759a6fe29fb1ad5325909c4d02952
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-11 17:12:56 -0600

    testing

--- backend/api/summary.py
+++ backend/api/summary.py
@@ -1,4 +1,5 @@
 import magic
+import PyPDF2
 
 
 def processfile(request):
@@ -6,12 +7,9 @@ def processfile(request):
         uploaded_file = request.FILES['file']
         mime = magic.Magic(mime=True)
         file_type = mime.from_buffer(uploaded_file.read(2048),mime=True)
-        text = totext(file_type)
-        
-        
-        # Proceed with processing...
+        text = toText(file_type)
 
-def totext(file_type):
+def toText(file_type):
     if file_type == 'text/plain':
             pass
     elif file_type == 'application/pdf':
@@ -20,3 +18,30 @@ def totext(file_type):
             pass
     else:
           pass
+
+
+
+
+
+
+def pdf_to_text(pdf_path):
+    try:
+          with open(pdf_path, 'rb') as pdf_file:
+               pdf_reader = PyPDF2.PdfReader(pdf_file)
+               text = ''
+               for page_num in range(len(pdf_reader.pages)):
+                page = pdf_reader.pages[page_num]
+                text += page.extract_text
+    except Exception as e:
+       return {'message': f'Error opening file type: {e}'}
+    return text
+    
+def txt_to_text(txt_path):
+    
+
+         
+               
+        
+         
+    
+
