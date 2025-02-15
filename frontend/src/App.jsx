3f210b854d4587c37a7859801e7fb1631bbb0b09
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-15 17:03:16 -0600

    l

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
+import react from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function App() {
-
-  return (
-    <>
-    </>
-  )
+function Logout() {
+  localStorage.clear();
+  return <Navigate to="/login" />;
 }
 
-export default App
+function RegisterandLogout() {
+  localStorage.clear();
+  return <Register />;
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route 
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Home/>
+          </ProtectedRoute>
+        }
+        />
+        <Route 
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
