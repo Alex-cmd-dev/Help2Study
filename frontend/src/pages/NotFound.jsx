fb8d048a3dd988042a96ac2c15ab5b95efa2b85e
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-16 21:07:34 -0600

    Tailwind and component library fixed

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,6 +3,7 @@ function NotFound() {
     <div>
       <h1>404</h1>
       <p>The page that you are looking for does not exist</p>
+      <progress className="progress progress-primary w-56" value="100" max="100"></progress>
     </div>
   );
 }
