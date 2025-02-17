ae84b3f4abf9bbe4fa0030d2783f45725f360be2
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-16 20:39:22 -0600

    package issue

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -3,6 +3,7 @@ function NotFound() {
     <div>
       <h1 className="font-extralight">404 Not Found</h1>
       <p>The page you're looking for does not exist</p>
+      <button className="btn btn-secondary">Secondary</button>
     </div>
   );
 }
