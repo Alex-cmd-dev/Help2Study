ee22cb7bc222603434a571cfb155d7be69fd4870
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-17 16:52:27 -0600

    fixed tailwind config

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -2,8 +2,7 @@ function NotFound() {
   return (
     <div>
       <h1>404</h1>
-      <p>The page that you are looking for does not exist</p>
-      <progress className="progress progress-primary w-56" value="100" max="100"></progress>
+      <p className="bg-red-500">The page that you are looking for does not exist</p>
     </div>
   );
 }
