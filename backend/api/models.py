6ea0989b8c79010c10410d45ca28af7afd2ec99e
Alexander Gallardo <alexgallardo@Alexanders-Laptop.local>
2025-02-22 18:50:15 -0600

    serializer done

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,19 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
-
+    def __str__(self):
+        return self.name
 
 
 class Flashcard(models.Model):
+    topic = models.ForeignKey(
+        Topic, on_delete=models.CASCADE, related_name="flashcards"
+    )
     question = models.TextField()
     answer = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="flashcards"
+        User, on_delete=models.CASCADE, related_name="user_flashcards"
     )  # deletes all the summaries the user has
 
     def __str__(self):
         return self.question
-
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,24 +1,31 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Summary
+from .models import Topic, Flashcard
 
-'''
+"""
 converts it from python to json
-'''
+"""
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id","username","password"]
-        extra_kwargs = {"password" : {"write_only": True}}
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
 
-class SummarySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Summary
-        fields = ["id","title","content","created_at","user"]
-        extra_kwargs = {"user":{"read_only":True}}
 
-class Flash
+class FlashcardSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = Flashcard
+        fields = ["id", "topic", "question", "answer", "created_at", "user"]
+        extra_kwargs = {"user": {"read_only": True}}
+
+
+class TopicSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ["id", "name", "created_at"]
